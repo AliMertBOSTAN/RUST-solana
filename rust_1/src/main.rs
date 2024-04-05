4af4f3d18ed8511ad7059943c67d59a12f19e5b0
Ali Mert BOSTAN <alimert930@gmail.com>
2024-04-05 21:44:41 +0300

    ders 2

--- rust_1/src/main.rs
+++ rust_1/src/main.rs
@@ -1,10 +1,17 @@
+use std::vec;
+
 
 
 fn main() {
     println!("Hello, world!");
 
-    let var: u8 = 1;
+    let mut var: u32 = 0;
     // let test: = 1; eğer belirtmezsem i32 olarak tanımlıyor
+    let v = vec!(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+
+    for i in v {
+        var += vec[i]
+    }
 
     println!("{}", var);
 }
